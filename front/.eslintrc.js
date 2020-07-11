08195682cf18a7522f1856ffc70f973c16a31752
kqito <29191111+kqito@users.noreply.github.com>
2020-07-12 01:49:18 +0900

    Update to resolve absolute path

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -55,5 +55,12 @@ module.exports = {
       },
     ],
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
-
--- front/next.config.js
+++ front/next.config.js
@@ -1,8 +1,26 @@
+/* eslint @typescript-eslint/no-var-requires: 0 */
+const path = require('path');
 require('dotenv').config();
+
+const webpackConfig = {
+  resolve: {
+    alias: {
+      '*': path.resolve(__dirname),
+    },
+  },
+};
 
 module.exports = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     TEST_VAR: process.env.TEST_VAR,
+  },
+  webpack: (baseConfig) => {
+    const config = baseConfig;
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...webpackConfig.resolve.alias,
+    };
+    return config;
   },
 };
