5d43ce40a3a08e770975d8106c61f71b53187106
kznrluk <kznr.luk@gmail.com>
2020-07-12 01:26:32 +0900

    デタッチでdocker-compose起動しない問題を解決

--- run-anyfrog.net.sh
+++ run-anyfrog.net.sh
@@ -6,11 +6,11 @@ bash ./app/build.sh
 bash ./front/build.sh
 
 #if type "docker-compose" > /dev/null 2>&1; then
-#  docker-compose -f ./deploy/gce/docker-compose-local.yaml up --build
+docker-compose -f ./deploy/gce/docker-compose-local.yaml up --build -d
 #else
-  sudo docker run \
-  --rm -v /var/run/docker.sock:/var/run/docker.sock \
-  -v "$PWD:/$PWD" -w="/$PWD" \
-  docker/compose:1.22.0 \
-  -f ./deploy/gce/docker-compose-local.yaml up --build -d
+#  sudo docker run \
+#  --rm -v /var/run/docker.sock:/var/run/docker.sock \
+#  -v "$PWD:/$PWD" -w="/$PWD" \
+#  docker/compose:1.22.0 \
+#  -f ./deploy/gce/docker-compose-local.yaml up --build -d
 #fi
