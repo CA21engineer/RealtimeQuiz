c542f7d09cf684a2cc3d98504a9469dec408f085
kqito <29191111+kqito@users.noreply.github.com>
2020-07-12 04:28:43 +0900

    Remove no longer used webpack config

--- front/next.config.js
+++ front/next.config.js
@@ -4,26 +4,10 @@ const withSass = require('@zeit/next-sass');
 
 require('dotenv').config();
 
-const webpackConfig = {
-  resolve: {
-    alias: {
-      '*': path.resolve(__dirname),
-    },
-  },
-};
-
 const config = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     TEST_VAR: process.env.TEST_VAR,
-  },
-  webpack: (baseConfig) => {
-    const newConfig = baseConfig;
-    newConfig.resolve.alias = {
-      ...newConfig.resolve.alias,
-      ...webpackConfig.resolve.alias,
-    };
-    return newConfig;
   },
 };
 
