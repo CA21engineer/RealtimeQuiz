be25ad87bd3e53d2b420e3b6d0d7c39f09fcda2e
dev-platong <platong8249@gmail.com>
2020-07-13 01:14:01 +0900

    結果発表待ち・結果発表画面の追加

--- front/src/pages/room/expressRoomStatus.ts
+++ front/src/pages/room/expressRoomStatus.ts
@@ -6,6 +6,11 @@ export function expressRoomStatus(status: ViewStatus): string {
       return '出題を待っています…';
     case ViewStatus.WAITING_ANSWER:
       return '解答オープンを待っています…';
+    case ViewStatus.CLOSE_ANSWER:
+    case ViewStatus.OPEN_ANSWER:
+      return '結果発表を待っています…';
+    case ViewStatus.OPEN_AGGRIGATE:
+      return '結果発表';
     default:
       throw new Error('Fatal: ユーザの状態が不明です．');
   }
