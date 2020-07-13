90e2041b43afe8474077043b7940f6c99f3923bd
kznrluk <kznr.luk@gmail.com>
2020-07-13 23:23:23 +0900

    コンフィグファイル追加

---
+++ front/src/configurations/Config.ts
@@ -0,0 +1,7 @@
+export const SERVER_HTTP_PROTOCOL = 'https';
+export const SERVER_WS_PROTOCOL = 'wss';
+
+export const SERVER_HOST = 'quiz.anyfrog.net';
+
+export const API_BASE_URL = `${SERVER_HTTP_PROTOCOL}://${SERVER_HOST}/api/`;
+export const WS_BASE_URL = `${SERVER_WS_PROTOCOL}://${SERVER_HOST}/ws/`;
