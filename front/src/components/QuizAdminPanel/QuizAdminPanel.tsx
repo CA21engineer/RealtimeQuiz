b86ddf52f2ebab0b7745d22dcfff863cd7ab3772
Kazuki Nagai <platong8249@gmail.com>
2020-07-13 23:08:13 +0900

    QuizAdminPanelの追加

---
+++ front/src/components/QuizAdminPanel/QuizAdminPanel.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './quizAdminPanel.scss';
+import { QuizPanel } from '../QuizPanel';
+
+type Props = {
+  name: string;
+  starNumber: number;
+  answerText: string;
+  givenStar: number;
+  setValue: (starNumber: number) => void;
+};
+
+export const QuizAdminPanel: React.FC<Props> = ({
+  name,
+  starNumber,
+  answerText,
+  setValue,
+}) => {
+  const [givenStar, setGivenStar] = React.useState(1);
+  return (
+    <div className="QuizAdminPanel__Container">
+      <QuizPanel name={name} starNumber={starNumber} answerText={answerText} />
+      <button
+        className="QuizAdminPanel__MinusStarButton"
+        onClick={() => setValue(givenStar)}
+        type="button"
+      >
+        {`⭐️-${givenStar}`}
+      </button>
+      <input
+        className="QuizAdminPanel__InputStar"
+        type="number"
+        value={givenStar}
+        onChange={(e) => {
+          setGivenStar(e.target.valueAsNumber);
+        }}
+      />
+      <button
+        className="QuizAdminPanel__PlusStarButton"
+        onClick={() => setValue(givenStar)}
+        type="button"
+      >
+        {`⭐️+${givenStar}`}
+      </button>
+    </div>
+  );
+};
--- front/src/pages/admin_room/index.tsx
+++ front/src/pages/admin_room/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './admin_room.scss';
 import { FoundationButton } from '../../components/FoundationButton';
-import { QuizPanel } from '../../components/QuizPanel';
+import { QuizAdminPanel } from '../../components/QuizAdminPanel';
 
 // FIXME: 仕様よう分かっとらんでな、あとで直す
 type User = {
@@ -10,9 +10,14 @@ type User = {
   input: string;
 };
 
+type UserStatus = {
+  user: User;
+  givenStar: number;
+};
+
 export const AdminRoom: React.FC = () => {
   const questionBody = '';
-  const users: User[] = [];
+  const userStatusArray: UserStatus[] = [];
   const remainTime = 0;
   return (
     <div className="AdminRoom__view">
@@ -33,12 +38,14 @@ export const AdminRoom: React.FC = () => {
           onClick={() => console.log('結果発表')}
         />
       </div>
-      {users.map((user) => (
-        <QuizPanel
-          key={user.name}
-          name={user.name}
-          starNumber={user.starNumber}
-          answerText={user.input}
+      {userStatusArray.map((userStatus) => (
+        <QuizAdminPanel
+          key={userStatus.user.name}
+          name={userStatus.user.name}
+          starNumber={userStatus.user.starNumber}
+          answerText={userStatus.user.input}
+          givenStar={userStatus.givenStar}
+          setValue={(starNumber: number) => console.log(starNumber)}
         />
       ))}
     </div>
