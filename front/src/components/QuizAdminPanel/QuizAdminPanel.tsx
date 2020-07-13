b45ee1dac51107fcf0581cd35524f32620bd071d
Kazuki Nagai <platong8249@gmail.com>
2020-07-13 23:28:00 +0900

    add QuizAdminPanel

--- front/src/components/QuizAdminPanel/QuizAdminPanel.tsx
+++ front/src/components/QuizAdminPanel/QuizAdminPanel.tsx
@@ -20,28 +20,30 @@ export const QuizAdminPanel: React.FC<Props> = ({
   return (
     <div className="QuizAdminPanel__Container">
       <QuizPanel name={name} starNumber={starNumber} answerText={answerText} />
-      <button
-        className="QuizAdminPanel__MinusStarButton"
-        onClick={() => setValue(givenStar)}
-        type="button"
-      >
-        {`⭐️-${givenStar}`}
-      </button>
-      <input
-        className="QuizAdminPanel__InputStar"
-        type="number"
-        value={givenStar}
-        onChange={(e) => {
-          setGivenStar(e.target.valueAsNumber);
-        }}
-      />
-      <button
-        className="QuizAdminPanel__PlusStarButton"
-        onClick={() => setValue(givenStar)}
-        type="button"
-      >
-        {`⭐️+${givenStar}`}
-      </button>
+      <div className="QuizAdminPanel__StarPanel">
+        <button
+          className="QuizAdminPanel__StarButton"
+          onClick={() => setValue(givenStar)}
+          type="button"
+        >
+          {`⭐️-${givenStar}`}
+        </button>
+        <input
+          className="QuizAdminPanel__InputStar"
+          type="number"
+          value={givenStar}
+          onChange={(e) => {
+            setGivenStar(e.target.valueAsNumber);
+          }}
+        />
+        <button
+          className="QuizAdminPanel__StarButton"
+          onClick={() => setValue(givenStar)}
+          type="button"
+        >
+          {`⭐️+${givenStar}`}
+        </button>
+      </div>
     </div>
   );
 };
