b3a13aa746265adc284ff580c39c8b0e271209b3
kznrluk <kznr.luk@gmail.com>
2020-07-15 00:00:42 +0900

    再接続機能の追加

--- front/src/connections/WSConnection.ts
+++ front/src/connections/WSConnection.ts
@@ -10,13 +10,30 @@ import { WS_BASE_URL } from '../configurations/Config';
 export class WSConnection extends Connection {
   private socket: WebSocket;
 
+  private url: string;
+
   constructor(connectionInfo: ConnectionInfo) {
     super();
 
-    this.socket = new WebSocket(
-      `${WS_BASE_URL}room/${connectionInfo.roomId}/accountId/${connectionInfo.accountId}`
-    );
+    this.url = `${WS_BASE_URL}room/${connectionInfo.roomId}/accountId/${connectionInfo.accountId}`;
+    this.socket = new WebSocket(this.url);
 
+    this.setEventHandlers();
+  }
+
+  // 再接続
+  private connect() {
+    this.socket = new WebSocket(this.url)
+    this.setEventHandlers();
+  }
+
+  // データを送信する
+  public emitData(type: string, data: unknown): void {
+    this.socket.send(JSON.stringify({ type, data }));
+  }
+
+  // ハンドラを設定し親クラスのcallFunctionを呼び出す
+  private setEventHandlers(): void {
     this.socket.addEventListener('open', () => {
       console.log('コネクション確立');
       this.callFunction('open', null);
@@ -26,16 +43,11 @@ export class WSConnection extends Connection {
       throw new Error('接続できませんでした');
     });
 
-    this.setMessageHandler();
-  }
+    this.socket.addEventListener('onclose', () => {
+      console.log('コネクションが切断されたため再度接続を試みます...');
+      this.connect();
+    })
 
-  // データを送信する
-  public emitData(type: string, data: unknown): void {
-    this.socket.send(JSON.stringify({ type, data }));
-  }
-
-  // ハンドラを設定し親クラスのcallFunctionを呼び出す
-  private setMessageHandler(): void {
     this.socket.addEventListener('message', (event) => {
       const json = JSON.parse(event.data);
       this.callFunction(json.type, json.data);
