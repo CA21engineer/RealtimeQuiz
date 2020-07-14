a4403e0a42ccda62867ab33c6af4e007107703ab
kznrluk <kznr.luk@gmail.com>
2020-07-14 18:29:45 +0900

    update script

--- run-anyfrog.net.sh
+++ run-anyfrog.net.sh
@@ -7,6 +7,9 @@ bash ./front/build.sh
 
 #if type "docker-compose" > /dev/null 2>&1; then
 docker-compose -f ./deploy/gce/docker-compose-local.yaml up --build -d
+
+cd front
+npm run start:ci &
 #else
 #  sudo docker run \
 #  --rm -v /var/run/docker.sock:/var/run/docker.sock \
