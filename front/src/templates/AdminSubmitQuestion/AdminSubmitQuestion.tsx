5d5f3a5bf7d9a45a322ff5626b34baf23c015023
kqito <29191111+kqito@users.noreply.github.com>
2020-07-14 20:12:07 +0900

    Add AdminSubmitQuestion

--- front/src/templates/AdminSubmitQuestion/AdminSubmitQuestion.tsx
+++ front/src/templates/AdminSubmitQuestion/AdminSubmitQuestion.tsx
@@ -1,38 +1,45 @@
-import React from 'react';
+import React, { useCallback, useContext, useRef } from 'react';
+import { GameStatusContext } from 'store/gameStatus';
 import { FoundationButton } from 'components/FoundationButton';
 import { QuizPanel } from 'components/QuizPanel';
 
 import './adminSubmitQuestion.scss';
 
-// FIXME: 仕様よう分かっとらんでな、あとで直す
-type User = {
-  name: string;
-  starNumber: number;
-  input: string;
-};
-
 export const AdminSubmitQuestion: React.FC = () => {
-  const questionBody = '';
-  const users: User[] = [];
+  const questionRef = useRef<HTMLInputElement>(null);
+  const { state } = useContext(GameStatusContext);
+  const { roomStatus, controllers } = state;
+  const { players } = roomStatus;
+
+  const submitQuestion = useCallback(() => {
+    const question = questionRef.current?.value;
+    if (!question || !controllers.emitter) {
+      return;
+    }
+
+    controllers.emitter.setQuestion(question);
+  }, [state, questionRef]);
+
   return (
     <div className="SubmitQuestion__view">
       <p>出題を待っています…</p>
       <div>
         <h1>問題を出題しています</h1>
-        <input className="Room__QuestionBox" value={questionBody} />
-        <FoundationButton
-          label="出題する"
-          onClick={() => console.log('出題する')}
-        />
+        <input className="Room__QuestionBox" type="text" ref={questionRef} />
+        <FoundationButton label="出題する" onClick={submitQuestion} />
       </div>
-      {users.map((user) => (
-        <QuizPanel
-          key={user.name}
-          name={user.name}
-          starNumber={user.starNumber}
-          answerText={user.input}
-        />
-      ))}
+
+      {players.map((player) => {
+        const answer = player.isAnswered ? '解答中...' : player.answer || '';
+        return (
+          <QuizPanel
+            key={player.id}
+            name={player.name}
+            starNumber={player.stars}
+            answerText={answer}
+          />
+        );
+      })}
     </div>
   );
 };
