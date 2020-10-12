182df2185df03daaf4a0dbb9a229dfe152ed5dc0
kqito <29191111+kqito@users.noreply.github.com>
2020-10-13 05:10:45 +0900

    Add support for mp3 file

--- front/next.config.js
+++ front/next.config.js
@@ -5,6 +5,22 @@ const withSass = require('@zeit/next-sass');
 require('dotenv').config();
 
 const config = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/sounds/',
+          outputPath: 'static/sounds/',
+          name: '[name].[ext]',
+          esModule: false,
+        },
+      },
+    });
+
+    return config;
+  },
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
     WS_BASE_URL: process.env.WS_BASE_URL,
